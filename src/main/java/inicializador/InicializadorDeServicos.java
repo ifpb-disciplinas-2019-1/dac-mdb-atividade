2d684f75b233437b7787662d13c3a7b82795c085
Isleimar Oliveira <isleimar@hotmail.com>
2019-08-02 08:12:45 -0300

    Criando o Recursos

---
+++ src/main/java/inicializador/InicializadorDeServicos.java
@@ -0,0 +1,23 @@
+package inicializador;
+
+import javax.jms.*;
+
+
+@JMSDestinationDefinitions({
+	@JMSDestinationDefinition(
+			name="java:global/jms/pedido",
+			interfaceName="javax.jms.Topic",
+			resourceAdapter="jmsra",
+			destinationName="topic"
+	),
+	@JMSDestinationDefinition(
+			name="java:global/jms/email",
+			interfaceName="javax.jms.Queue",
+			resourceAdapter="jmsra",
+			destinationName="queue"
+	)
+})
+
+public class InicializadorDeServicos {
+
+}
