e6583e1ff61ef8523976d79b5a5d63484e29dd06
Luan <luanbruno974@gmail.com>
2019-08-02 09:32:39 -0300

    Implementando métodos da interface pedido

--- src/main/java/edu/ifpb/dac/dao/PedidoJPADAO.java
+++ src/main/java/edu/ifpb/dac/dao/PedidoJPADAO.java
@@ -3,26 +3,32 @@ package edu.ifpb.dac.dao;
 import edu.ifpb.dac.Pedido;
 import edu.ifpb.dac.dao.jpa.PedidoDAO;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 public class PedidoJPADAO  implements PedidoDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void save(Pedido pedido) {
-
+        em.persist(pedido);
     }
 
     @Override
     public void update(Pedido pedido) {
-
+        em.merge(pedido);
     }
 
     @Override
     public void remove(Pedido pedido) {
-
+        em.remove(pedido);
     }
 
     @Override
     public List<Pedido> list() {
-        return null;
+        return em.createQuery("SELECT p FROM Pedido p").getResultList();
     }
 }
