5c28ca6c0efcf303d1a885022776af9381dbb01c
antonio <antoniofilhonerde@gmail.com>
2019-08-07 11:13:17 -0300

    inicinado bean pedido

--- src/main/java/edu/ifpb/controlador/NovoPedido.java
+++ src/main/java/edu/ifpb/controlador/NovoPedido.java
@@ -3,18 +3,24 @@ package edu.ifpb.controlador;
 import edu.ifpb.dac.Cliente;
 import edu.ifpb.dac.Pedido;
 import edu.ifpb.dac.Produto;
+import edu.ifpb.dac.dao.jpa.PedidoDAO;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.List;
 
 /**
  *
  * @author ericl
  */
+@SuppressWarnings("/serial/")
+@RequestScoped
+@Named
 public class NovoPedido {
-    
-    public Pedido criarPedido(Cliente cliente, List<Produto> produtos){
-        Pedido pedido = new Pedido();
-        pedido.setCliente(cliente);
-        pedido.setProdutos(produtos);
-        return pedido;
-    }
+
+    @Inject
+    PedidoDAO pedidoDAO;
+
+    public 
 }
